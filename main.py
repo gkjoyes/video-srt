22247693d85f0ec687d548fb0b9bd7465ee7b75a
qburst-georgekutty <georgekutty@qburst.com>
2019-01-27 12:58:51 +0530

    Reading of subtitle csv file completed

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import toml
+import csv
 
 
 def read_conf(file="conf/conf.toml"):
+    """
+    read_conf will read configfile for our project
+    """
+
     try:
         with open(file) as configfile:
             confstr = configfile.read()
@@ -10,7 +15,28 @@ def read_conf(file="conf/conf.toml"):
         print('Conf File not found. Check the path variable and filename: ', str(e))
 
 
+def read_csv(file_path='data/subtitle.csv'):
+    """
+    read_csv will read subtitle csv file
+    and convert that data into a specific format for
+    compatibility with moviepy library.
+    """
+
+    lines = []
+    with open(file_path, newline='') as subtitles:
+        subtitle_reader = csv.DictReader(subtitles)
+        for txt in subtitle_reader:
+            line = []
+            start_end = (int(txt['start']), int(txt['end']))
+            line.append(start_end)
+            line.append(txt['text'])
+            lines.append(tuple(line))
+
+    return lines
+
+
 if __name__ == '__main__':
     conf_file = "conf/conf.toml"
     conf = read_conf(conf_file)
-    print(conf)
+    subtitles = read_csv(conf['data']['subtitle'])
+    print(subtitles)
