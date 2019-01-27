8f1b207035c840c3a55c37e764ece29aa32b87dc
qburst-georgekutty <georgekutty@qburst.com>
2019-01-27 13:34:25 +0530

    Adding subtitle to video using moviepy completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import toml
 import csv
+from moviepy import editor
+import sys
 
 
 def read_conf(file="conf/conf.toml"):
@@ -13,6 +15,7 @@ def read_conf(file="conf/conf.toml"):
         return toml.loads(confstr)
     except Exception as e:
         print('Conf File not found. Check the path variable and filename: ', str(e))
+        sys.exit()
 
 
 def read_csv(file_path='data/subtitle.csv'):
@@ -21,22 +24,52 @@ def read_csv(file_path='data/subtitle.csv'):
     and convert that data into a specific format for
     compatibility with moviepy library.
     """
+    try:
+        lines = []
+        with open(file_path, newline='') as subtitles:
+            subtitle_reader = csv.DictReader(subtitles)
+            for txt in subtitle_reader:
+                line = []
+                start_end = (int(txt['start']), int(txt['end']))
+                line.append(start_end)
+                line.append(txt['text'])
+                lines.append(tuple(line))
 
-    lines = []
-    with open(file_path, newline='') as subtitles:
-        subtitle_reader = csv.DictReader(subtitles)
-        for txt in subtitle_reader:
-            line = []
-            start_end = (int(txt['start']), int(txt['end']))
-            line.append(start_end)
-            line.append(txt['text'])
-            lines.append(tuple(line))
+        return lines
+    except Exception as e:
+        print('Subtitle File not found. Check the path variable and filename: ', str(e))
+        sys.exit()
 
-    return lines
+
+def annotate(
+        clip,
+        subtitle,
+        txt_color='white',
+        fontsize=20,
+        font='',
+        loc_x="center",
+        loc_y="bottom"):
+    """
+    Writes a text(subtitle) to the specified position.
+    """
+    txtclip = editor.TextClip(subtitle, fontsize=fontsize, font=font, color=txt_color)
+    cvc = editor.CompositeVideoClip([clip, txtclip.set_pos((loc_x, loc_y))])
+    return cvc.set_duration(clip.duration)
+
+
+def append_subtitle(conf, subtitles):
+    try:
+        video = editor.VideoFileClip(conf['data']['video'])
+        annotated_clips = [annotate(video.subclip(from_t, to_t), txt, conf['subtitle-conf']['txt_color'])
+                           for (from_t, to_t), txt in subtitles]
+        final_clip = editor.concatenate_videoclips(annotated_clips)
+        final_clip.write_videofile(conf['data']['output'])
+    except Exception as e:
+        print("Exception occured during appending subtitle to video: ", str(e))
 
 
 if __name__ == '__main__':
     conf_file = "conf/conf.toml"
     conf = read_conf(conf_file)
     subtitles = read_csv(conf['data']['subtitle'])
-    print(subtitles)
+    append_subtitle(conf, subtitles)
